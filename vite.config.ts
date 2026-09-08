e6685099120d2a58a23c32a709174161094a1fd2
Ruilin <ruilin@example.com>
2026-09-08 12:47:39 +0800

    Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; export default defineConfig({ plugins: [react()], resolve: { alias: { "@": "./src" } }, css: { postcss: "postcss.config.cjs" } })
+import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; export default defineConfig({ plugins: [react()], resolve: { alias: { "@": "./src" } }, base: "/data-science-lab/", css: { postcss: "postcss.config.cjs" } })
